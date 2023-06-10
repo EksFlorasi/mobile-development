a0185078854f717c00ca0c828a146ac4e8e143ff
Frans Wijaya <franswijaya403@gmail.com>
2023-06-10 23:19:24 +0700

    Create splash and landing screen
    The color configuration is correct, but when previewed, the color changed.

--- app/src/main/java/com/example/eksflorasi/ui/authentication/LandingScreen.kt
+++ app/src/main/java/com/example/eksflorasi/ui/authentication/LandingScreen.kt
@@ -1,16 +1,18 @@
 package com.example.eksflorasi.ui.authentication
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,7 +23,7 @@ import com.example.eksflorasi.ui.theme.EksFlorasiTheme
 import com.example.eksflorasi.ui.theme.PoppinsFamily
 
 @Composable
-fun SplashScreen() {
+fun LandingScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -51,14 +53,52 @@ fun SplashScreen() {
                 color = MaterialTheme.colorScheme.primary,
                 modifier = Modifier.padding(top = 16.dp),
             )
+            Spacer(modifier = Modifier.height(80.dp))
+            Button(
+                onClick = { /* Button click action */ },
+                modifier = Modifier
+                    .height(56.dp)
+                    .width(216.dp),
+                shape = MaterialTheme.shapes.large,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Text(
+                    text = stringResource(R.string.login),
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedButton(
+                onClick = { /* Button click action */ },
+                modifier = Modifier
+                    .height(56.dp)
+                    .width(216.dp),
+                shape = MaterialTheme.shapes.large,
+                colors = ButtonDefaults.outlinedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.onPrimary
+                ),
+                border = BorderStroke(
+                    color = MaterialTheme.colorScheme.primary,
+                    width = 4.dp
+                )
+            ) {
+                Text(
+                    text = stringResource(R.string.signup),
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun SplashScreenPreview() {
+fun LandingScreenPreview() {
     EksFlorasiTheme {
-        SplashScreen()
+        LandingScreen()
     }
 }
--- app/src/main/java/com/example/eksflorasi/ui/theme/Type.kt
+++ app/src/main/java/com/example/eksflorasi/ui/theme/Type.kt
@@ -10,9 +10,12 @@ import androidx.compose.ui.unit.sp
 import com.example.eksflorasi.R
 
 val PoppinsFamily = FontFamily(
-    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
     Font(R.font.poppins_bold, FontWeight.Bold),
     Font(R.font.poppins_extrabold, FontWeight.ExtraBold),
+    Font(R.font.poppins_italic, FontWeight.Normal, FontStyle.Italic),
     Font(R.font.poppins_light, FontWeight.Light),
     Font(R.font.poppins_lightitalic, FontWeight.Light, FontStyle.Italic)
 )
@@ -20,73 +23,84 @@ val PoppinsFamily = FontFamily(
 // Set of Material typography styles to start with
 val Typography = Typography(
     headlineLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = PoppinsFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = PoppinsFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     bodyLarge = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.15.sp
+        letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.25.sp
+        letterSpacing = 0.1.sp
     ),
     bodySmall = TextStyle(
-        fontWeight = FontWeight.Bold,
+        fontFamily = PoppinsFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp
     ),
     labelLarge = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
+        fontFamily = PoppinsFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 11.sp,
         lineHeight = 16.sp,
